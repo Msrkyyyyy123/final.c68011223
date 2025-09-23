7b99efa7404a28d4bc3312552230743408a0a20c
Msrkyyyyy123 <anuwuth.ch@gmail.com>
2025-09-23 16:24:39 +0700

    Add files via upload

---
+++ main.c.final.new.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    // ประกาศตัวแปร
+    int a = 15, b = 10;
+    
+    // ตัวดำเนินการคณิตศาสตร์
+    int sum = a + b;       // ผลบวก
+    int diff = a - b;      // ผลต่าง
+    int mult = a * b;   // ผลคูณ
+    int div = a / b;  // ผลหาร (ใช้ float จะเหมาะสมกว่า)
+    int remain = a % b; // ผลหารเศษ
+    
+    // แสดงผลลัพธ์
+    printf("Sum: %d\n", sum);
+    printf("Difference: %d\n", diff);
+    printf("Product: %d\n", mult);
+    printf("Quotient: %d\n", div);
+    printf("Remainder: %d\n", remain);
+    
+    printf("hello\n");
+    printf("hellooo\n");
+    printf("hellooooooo\n");
+    printf("hellooooooooooo\n");
+    printf("hellooooooooooooooooo\n");
+
+    return 0;
+}
